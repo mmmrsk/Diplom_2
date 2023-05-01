13a41a211dce6c9b9f0b6320af158a2d1f7d29fc
mmmrsk <f55651068@gmail.com>
2023-05-01 21:02:11 +0300

    adding tests for GetOrder and UserLogin

--- src/test/java/ru/yandex/praktikum/stellarburgers/CreateOrderTest.java
+++ src/test/java/ru/yandex/praktikum/stellarburgers/CreateOrderTest.java
@@ -1,5 +1,6 @@
 package ru.yandex.praktikum.stellarburgers;
 
+import io.qameta.allure.Description;
 import ru.yandex.praktikum.stellarburgers.clients.IngredientClient;
 import ru.yandex.praktikum.stellarburgers.clients.OrderClient;
 import ru.yandex.praktikum.stellarburgers.clients.UserClient;
@@ -45,18 +46,10 @@ public class CreateOrderTest {
         accessToken = responseForToken.then().extract().path("accessToken");
 
     }
-
-    @Test
-    @DisplayName("Create order by unauthorized user")
-    public void createOderWithoutAuthorizationTest() {
-        oderClient = new OrderClient();
-        oder = new Order(ingredients);
-        response = oderClient.createOrderByUnauthorizedUser(oder);
-        response.then().assertThat().statusCode(200).and().body("success", equalTo(true));
-    }
-
+    // Тест 1: создание заказа с авторизацией
     @Test
     @DisplayName("Create order by authorized user")
+    @Description("Positive test, a successful server response is 200, the response body contains success: true")
     public void createOderWithAuthorizationTest() {
         oder = new Order(ingredients);
         oderClient = new OrderClient();
@@ -64,9 +57,20 @@ public class CreateOrderTest {
         response.then().assertThat().body("success", equalTo(true)).and().statusCode(200);
 
     }
-
+    // Тест 2: создание заказа без авторизации
+    @Test
+    @DisplayName("Create order by unauthorized user")
+    @Description("Positive test, a successful server response is 200, the response body contains success: true")
+    public void createOderWithoutAuthorizationTest() {
+        oderClient = new OrderClient();
+        oder = new Order(ingredients);
+        response = oderClient.createOrderByUnauthorizedUser(oder);
+        response.then().assertThat().statusCode(200).and().body("success", equalTo(true));
+    }
+    // Тест 3: создание заказа с неверным хешем ингредиентов
     @Test
     @DisplayName("Create order with wrong hash ingredients")
+    @Description("Negative test, for a request with wrong hash ingredients, the system responds with a 500 code")
     public void createOderWithIncorrectIngredient() {
 
         ingredients.add("Test");
@@ -75,9 +79,10 @@ public class CreateOrderTest {
         response = oderClient.createOrderByAuthorizedUser(accessToken, oder);
         response.then().assertThat().statusCode(500);
     }
-
+    // Тест 4: создание заказа без ингредиентов
     @Test
     @DisplayName("Create order without ingredients")
+    @Description("Negative test, for a request without ingredients, the system responds with a 400 code")
     public void createOderWithNullIngredient() {
 
         ingredients.clear();
---
+++ src/test/java/ru/yandex/praktikum/stellarburgers/UserLoginTest.java
@@ -0,0 +1,55 @@
+package ru.yandex.praktikum.stellarburgers;
+
+import io.qameta.allure.Description;
+import ru.yandex.praktikum.stellarburgers.clients.UserClient;
+import io.qameta.allure.junit4.DisplayName;
+import io.restassured.response.Response;
+import ru.yandex.praktikum.stellarburgers.models.User;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.equalTo;
+
+public class UserLoginTest {
+    public UserClient userClient;
+    public User user;
+    Response response;
+
+    @Before
+    public void setup() {
+        userClient = new UserClient();
+        user = User.getRandomUser();
+        userClient.createUser(user);
+    }
+    // Тест 1: логин под существующим пользователем
+    @Test
+    @DisplayName("Login user with all required fields")
+    @Description("Positive test, a successful server response is 200, the response body contains success: true")
+    public void testUserLogin() {
+        response = userClient.loginUser(user);
+        response.then().assertThat().body("success", equalTo(true)).and().statusCode(200);
+    }
+    // Тест 2: логин с неверным логином
+    @Test
+    @DisplayName("Login user with wrong login")
+    @Description("Negative test, for a request with an incorrect email, the system responds with a 401 code")
+    public void testUserLoginWithIncorrectEmail() {
+        user.setEmail("test");
+        response = userClient.loginUser(user);
+        response.then().assertThat().body("success", equalTo(false)).and().statusCode(401);
+    }
+    // Тест 3: логин с неверным паролем
+    @Test
+    @DisplayName("Login user with wrong password")
+    @Description("Negative scenario, for a request with an incorrect password, the system responds with a 401 code")
+    public void testUserLoginWithIncorrectPassword() {
+        user.setPassword("test");
+        response = userClient.loginUser(user);
+        response.then().assertThat().body("success", equalTo(false)).and().statusCode(401);
+    }
+    @After
+    public void deleteUser() {
+        userClient.deleteUser();
+    }
+}
